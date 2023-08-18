97e403e6218a41f44d5abefd053a649330b7f016
Moukarnold <moukarnold@gmail.com>
2023-08-18 02:47:14 +0200

    styling the way a movie is display in background

--- js/script.js
+++ js/script.js
@@ -72,13 +72,43 @@ const global = {
     return data;
   }
   
+  function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.3';
+  
+    if (type === 'movie') {
+      document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+      document.querySelector('#show-details').appendChild(overlayDiv);
+    }
+  }
+
+
+
   // display Movies details
 
   async function displayMoviesDetails() {
 
   const movieId = window.location.search.split("=")[1];
+
+ 
   const cardShowDetail = document.querySelector("#movie-details");
   const movie = await fetchAPIdata(`movie/${movieId}`);
+
+       // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
+
   const div = document.createElement("div");
   div.innerHTML = `
     <div class="details-top">
